3738d01580c600c18490a46bd7861911dec533f8
yashchks87 <yashchks87@gmail.com>
2023-03-07 19:27:24 +0000

    Upto base model is fixed.

---
+++ handle_data.sh
@@ -0,0 +1,11 @@
+# Please make sure to export kaggle user name and key from json file structure.
+
+mkdir data
+
+cd data/
+
+kaggle competitions download -c plant-pathology-2021-fgvc8
+
+unzip plant-pathology-2021-fgvc8.zip
+
+cd ../
