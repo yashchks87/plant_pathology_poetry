82e39d08533687a42590561e4a617472db6ff215
yashchke87 <yashchks87@gmail.com>
2023-03-07 00:55:02 +0000

    Final update from labda labs VM.

---
+++ test.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+
+print(
+    "ldfkjgdflkgjldkfgjldfkjgdflkgjldkfgjldfkjgdflkgjldkfgjldfkjgdflkgjldkfgjldfkjgdflkgjldkfgjldfkjgdflkgjldkfgjldfkjgdflkgjldkfgjldfkjgdflkgjldkfgjldfkjgdflkgjldkfgjldfkjgdflkgjldkfgjldfkjgdflkgjldkfgjldfkjgdflkgjldkfgj"
+)
